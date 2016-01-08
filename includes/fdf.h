59f676a2aacad16e7bb98c3a49d28d090d3fe3f8
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-01-08 14:02:03 +0100

    Fix perspective issue

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 10:07:19 by tguillem          #+#    #+#             */
-/*   Updated: 2016/01/06 11:06:38 by tguillem         ###   ########.fr       */
+/*   Updated: 2016/01/08 11:03:20 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 # include <mlx.h>
 # include <stdlib.h>
 
-typedef struct		s_env
+typedef struct	s_env
 {
-	void			*mlx;
-	void			*win;
-}					t_env;
+	void		*mlx;
+	void		*win;
+}				t_env;
 
-typedef struct		s_vec3
+typedef struct	s_vec3
 {
-	int				x;
-	int				y;
-	int				z;
-}					t_vec3;
+	int			x;
+	int			y;
+	int			z;
+}				t_vec3;
 
-void				draw_quad(t_env *env, t_vec3 *pos, int size, int color);
-t_vec3				*new_vec3(int x, int y, int z);
-int					expose_hook(void *param);
+void			draw_quad(t_env *env, t_vec3 *pos, int size, int color);
+void			draw_line(t_env *env, t_vec3 *start, t_vec3 *end, int color);
+t_vec3			*new_vec3(int x, int y, int z);
+int				expose_hook(void *param);
 #endif
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 09:59:30 by tguillem          #+#    #+#             */
-/*   Updated: 2016/01/06 15:00:44 by tguillem         ###   ########.fr       */
+/*   Updated: 2016/01/08 14:01:40 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #define PROJ_X(X, Y, Z) ((X/2) - (Y/2) + 200)
 #define PROJ_Y(X, Y, Z) (-(Z/2) + (X/4) + (Y/4) + 100)
+#define ABS(X) X < 0 ? (-X) : X
 void		draw_quad(t_env *env, t_vec3 *pos, int size, int color)
 {
 	int i;
@@ -29,19 +30,19 @@ void		draw_quad(t_env *env, t_vec3 *pos, int size, int color)
 		{
 			if (i == 1 || i == size || j == 1 || j == size)
 			{
-				x = pos->x + i;
-				y = pos->y + j;
-				if (pos->z)
-				{
-					x = PROJ_X(x, y, pos->z);
-					y = PROJ_Y(x, y, pos->z);
-				}
+				x = PROJ_X(pos->x + i, pos->y + j, pos->z);
+				y = PROJ_Y(pos->x, pos->y + j, pos->z);
 				mlx_pixel_put(env->mlx, env->win, x, y, color);
 			}
 			j++;
 		}
 		i++;
 	}
+}
+
+void		draw_line(t_env *env, t_vec3 *start, t_vec3 *end, int color)
+{
+
 }
 
 t_vec3		*new_vec3(int x, int y, int z)

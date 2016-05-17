90aa4ae533d397750eba646b1dc921a6f5ad6948
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-05-17 13:00:04 +0200

    Avoid malloc and free in rendering loop

--- src/hooks.c
+++ src/hooks.c
@@ -6,25 +6,24 @@
 /*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 10:41:55 by tguillem          #+#    #+#             */
-/*   Updated: 2016/05/17 12:38:43 by tguillem         ###   ########.fr       */
+/*   Updated: 2016/05/17 12:59:24 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-t_pos	*get_and_transform_pos(t_env *env, int x, int y)
+int		get_and_transform_pos(t_env *env, t_pos *pos, int x, int y)
 {
-	t_pos	*pos;
+	t_pos	*tmp;
 
-	if ((pos = get_pos(env->data, x, y)))
-		pos = new_pos(x, y, pos->z);
-	if (pos)
+	tmp = get_pos(env->data, x, y);
+	if (tmp)
 	{
-		pos->x = (pos->x + env->modifier->x) * W_SC + W_SC + 1000;
-		pos->y = (pos->y + env->modifier->y) * W_SC + W_SC;
-		pos->z = pos->z * H_SC;
+		pos->x = (tmp->x + env->modifier->x) * W_SC + W_SC + 1000;
+		pos->y = (tmp->y + env->modifier->y) * W_SC + W_SC;
+		pos->z = tmp->z * H_SC;
 	}
-	return (pos);
+	return (tmp != NULL);
 }
 
 void	display_vertical(t_env *env)
@@ -36,19 +35,21 @@ void	display_vertical(t_env *env)
 
 	i = 0;
 	j = 0;
-	while ((tmp1 = get_pos(env->data, i, j)))
+	tmp1 = new_pos(0, 0, 0);
+	tmp2 = new_pos(0, 0, 0);
+	while (get_pos(env->data, i, j) && tmp1 && tmp2)
 	{
 		j = 0;
-		while ((tmp1 = get_and_transform_pos(env, i, j)) &&
-			(tmp2 = get_and_transform_pos(env, i, j + 1)))
+		while (get_and_transform_pos(env, tmp1, i, j) &&
+			get_and_transform_pos(env, tmp2, i, j + 1))
 		{
 			draw_line_3d(env, tmp1, tmp2, 0xFFFFFF);
-			free(tmp1);
-			free(tmp2);
 			++j;
 		}
 		++i;
 	}
+	ft_memdel((void **)&tmp1);
+	ft_memdel((void **)&tmp2);
 }
 
 void	display_horizontal(t_env *env)
@@ -60,19 +61,21 @@ void	display_horizontal(t_env *env)
 
 	i = 0;
 	j = 0;
-	while ((tmp1 = get_pos(env->data, i, j)))
+	tmp1 = new_pos(0, 0, 0);
+	tmp2 = new_pos(0, 0, 0);
+	while (get_pos(env->data, i, j))
 	{
 		i = 0;
-		while ((tmp1 = get_and_transform_pos(env, i, j)) &&
-			(tmp2 = get_and_transform_pos(env, i + 1, j)))
+		while ((get_and_transform_pos(env, tmp1, i, j)) &&
+				get_and_transform_pos(env, tmp2, i + 1, j))
 		{
 			draw_line_3d(env, tmp1, tmp2, 0xFFFFFF);
-			free(tmp1);
-			free(tmp2);
 			++i;
 		}
 		++j;
 	}
+	ft_memdel((void **)&tmp1);
+	ft_memdel((void **)&tmp2);
 }
 
 int		expose_hook(void *param)
@@ -94,7 +97,9 @@ int		key_hook(int keycode, void *param)
 	t_env		*env;
 
 	env = (t_env*)param;
-	if (env)
+	if (keycode == ESCAPE)
+		exit(EXIT_SUCCESS);
+	else if (env)
 	{
 		if (keycode == ARROW_DOWN)
 			env->modifier->y++;
@@ -107,7 +112,5 @@ int		key_hook(int keycode, void *param)
 		if (keycode > ARROW_OFFSET && keycode < (ARROW_OFFSET + 5))
 			expose_hook(param);
 	}
-	if (keycode == ESCAPE)
-		exit(EXIT_SUCCESS);
 	return (env == NULL);
 }
